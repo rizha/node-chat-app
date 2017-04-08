e7d1b8002bfc24cf55231702a936d0cf4ede2b83
rizha <rizha.musthafa@gmail.com>
2017-04-08 18:15:46 +0700

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,6 @@
 let socket = io();
 socket.on('connect', () => {
   console.log('connected to the server');
-
-  socket.emit('createMessage', {
-    from: 'rizha',
-    text: 'Yup, that works for me.'
-  });
 });
 
 socket.on('disconnect', () => {
--- server/server.js
+++ server/server.js
@@ -15,18 +15,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('new user connected');
 
-  socket.emit('newMessage', {
-    from: 'john',
-    text: 'See you then',
-    createdAt: 1234
-  });
-
-  socket.on('createEmail', (newEmail) => {
-    console.log('createEmail ', newEmail);
-  });
-
+  
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
